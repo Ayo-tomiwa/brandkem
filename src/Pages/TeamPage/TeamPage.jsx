bdab59ee3214c3b8b4d1e8fc532378f67902587c
Ayo-tomiwa <oyindml1@gmail.com>
2026-05-12 13:57:52 +0100

    Remove unused imports

--- src/Pages/TeamPage/TeamPage.jsx
+++ src/Pages/TeamPage/TeamPage.jsx
@@ -1,15 +1,11 @@
 import React from "react";
+import "./TeamPage.css";
 import TeamHero from "../../Components/Pages/Team/TeamHero/TeamHero";
 import TeamCarousel from "../../Components/Pages/Home/Team/TeamCarousel/TeamCarousel";
 
-import AOS from "aos";
-import "aos/dist/aos.css";
-
-AOS.init();
-
 const TeamPage = () => {
   return (
-    <div>
+    <div className="team-page">
       <TeamHero />
       <TeamCarousel />
     </div>
